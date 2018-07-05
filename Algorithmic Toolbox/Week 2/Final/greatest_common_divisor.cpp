22d05772301ed30eae9667997047154329569f4c
minikin <djminikin@gmail.com>
2018-07-05 13:58:31 +0200

    Update huge_fibonacci_modulo_m

---
+++ Algorithmic Toolbox/Week 2/Final/greatest_common_divisor.cpp
@@ -0,0 +1,17 @@
+// "Copyright [2018] <Sasha Prokhorenko"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long greatest_common_divisor(long long n) {
+
+}
+
+int main() {
+    long long n;
+    cin >> n;
+
+    cout << greatest_common_divisor(n) << '\n';
+    return 0;
+}
--- Algorithmic Toolbox/Week 2/Final/huge_fibonacci_modulo_m.cpp
+++ Algorithmic Toolbox/Week 2/Final/huge_fibonacci_modulo_m.cpp
@@ -1,34 +1,30 @@
 // "Copyright [2018] <Sasha Prokhorenko"
 #include <iostream>
 #include <vector>
-#include <cstdlib>
 
 using namespace std;
 
-long long pisano_period(long long modulo) {
-    long long previous = 0;
-    long long current = 1;
-    long long result = 1;
+long long pisano_period(long long m) {
+    long long a = 0;
+    long long  b = 1;
+    long long c = a + b;
 
-    while ((previous) != 0 && (current != 1)) {
-        long long buffer = (previous + current) % modulo;
-        previous = current;
-        current = buffer;
-        result += 1;
+    for (int i = 0; i < m * m; i++) {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+        if (a == 0 && b == 1) return i + 1;
     }
-
-    return result;
 }
 
-long long get_fibonacci_huge(long long n, long long m) {
+long long fibonacci_huge_modulo(long long n, long long m) {
     long long remainder = n % pisano_period(m);
 
     long long first = 0;
     long long second = 1;
-
     long long res = remainder;
 
-    for (int i = 1; i < remainder; i++) {
+    for (long long i = 1; i < remainder; i++) {
         res = (first + second) % m;
         first = second;
         second = res;
@@ -37,29 +33,9 @@ long long get_fibonacci_huge(long long n, long long m) {
     return res % m;
 }
 
-
-// long long get_fibonacci_huge(long long n, long long m) {
-//     if (n == 1) {
-//         return 1;
-//     } else {
-//         if ((m == 0) || (m == n)) {
-//             return 0;
-//         } else {
-//             std::vector<long long> numbers(n+1);
-//             numbers[0] = 0;
-//             numbers[1] = 1;
-
-//             for (long long i = 2; i <= n; ++i) {
-//                 numbers[i] = (numbers[i - 1] + numbers[i - 2]) % m;
-//             }
-//             return numbers[n];
-//         }
-//     }
-// }
-
 int main() {
     long long n, m;
     cin >> n >> m;
-    cout << get_fibonacci_huge(n, m) << '\n';
+    cout << fibonacci_huge_modulo(n, m) << '\n';
     return 0;
 }
