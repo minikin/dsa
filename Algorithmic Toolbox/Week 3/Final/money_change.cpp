994c262edf7f3ab2fcfab19f1447fac84e03833e
minikin <djminikin@gmail.com>
2018-07-12 22:25:30 +0200

    Minor update

---
+++ Algorithmic Toolbox/Week 3/Final/money_change.cpp
@@ -0,0 +1,11 @@
+// "Copyright [2018] <Sasha Prokhorenko"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main() {
+    return 0;
+}
---
+++ Algorithmic Toolbox/Week 3/Naive/money_change.cpp
@@ -0,0 +1,12 @@
+// "Copyright [2018] <Sasha Prokhorenko"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main() {
+    return 0;
+}
