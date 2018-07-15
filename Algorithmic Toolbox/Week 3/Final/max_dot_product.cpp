385574f6d920a29199f085aaad7f6a531a5e68cd
minikin <djminikin@gmail.com>
2018-07-15 11:22:24 +0200

    Update max_dot_product.cpp

--- Algorithmic Toolbox/Week 3/Final/max_dot_product.cpp
+++ Algorithmic Toolbox/Week 3/Final/max_dot_product.cpp
@@ -6,8 +6,10 @@
 using namespace std;
 
 long long max_dot_product(vector<int> a, vector<int> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end(), std::greater<int>());
+
+    sort(a.begin(), a.end(), greater<int>());
+    sort(b.begin(), b.end(), greater<int>());
+
     long long result = 0;
     for (int i = 0; i < a.size(); i++) {
         result += (long long) a[i] * b[i];
