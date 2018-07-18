5b61199de86091b315b3792894c59fe597ecb685
minikin <djminikin@gmail.com>
2018-07-18 14:57:57 +0200

    Add week 4

---
+++ Algorithmic Toolbox/Week 4/Final/binary_search.cpp
@@ -0,0 +1,14 @@
+// "Copyright [2018] <Sasha Prokhorenko"
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+double binary_search() {
+    return 0;
+}
+
+int main() {
+  return 0;
+}
