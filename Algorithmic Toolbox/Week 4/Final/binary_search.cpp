6d706082dc65179584e1e4f694cbf067b5a69cc7
minikin <djminikin@gmail.com>
2018-07-21 19:20:24 +0200

    Add binary search, majority_elementmajority_element, majority_element

--- Algorithmic Toolbox/Week 4/Final/binary_search.cpp
+++ Algorithmic Toolbox/Week 4/Final/binary_search.cpp
@@ -1,14 +1,42 @@
 // "Copyright [2018] <Sasha Prokhorenko"
 #include <iostream>
+#include <cassert>
 #include <vector>
-#include <cstdlib>
 
 using namespace std;
 
-double binary_search() {
-    return 0;
+int binary_search_iterative(const vector<int> &a, int x) {
+    int left = 0, right = (int) a.size();
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (x == a[mid]) return mid;
+        else if (x < a[mid]) right = mid - 1;
+        else left = mid + 1;
+    }
+    return -1;
+}
+
+int linear_search(const vector<int> &a, int x) {
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] == x) return i;
+    }
+    return -1;
 }
 
 int main() {
-  return 0;
+    int n;
+    std::cin >> n;
+    vector<int> a(n);
+    for (size_t i = 0; i < a.size(); i++) {
+        std::cin >> a[i];
+    }
+    int m;
+    std::cin >> m;
+    vector<int> b(m);
+    for (int i = 0; i < m; ++i) {
+        std::cin >> b[i];
+    }
+    for (int i = 0; i < m; ++i) {
+        std::cout << binary_search_iterative(a, b[i]) << ' ';
+    }
 }
